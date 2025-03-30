c10a21a6a6b100d6293920e5b3e3acecd0577d44
Nanta Es <ombakrinai@gmail.com>
2025-03-30 16:18:34 +0700

    update API_URL_RENDER

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -19,7 +19,7 @@ use yii\httpclient\Client;
 class SiteController extends Controller
 {
     const API_URL_LOCALHOST = 'http://127.0.0.1:8000/ask';
-    const API_URL_RENDER = 'https://ai-chatbot-informasi-dana-bos.onrender.com/ask';
+    const API_URL_RENDER = 'https://ai-chatbot-informasi-publik-berbasis.onrender.com/ask';
 
     /**
      * {@inheritdoc}
